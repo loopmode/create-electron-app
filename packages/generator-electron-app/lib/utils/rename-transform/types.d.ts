12f10184c8854d333e9de515b6641d9190b7c24e
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-08-18 15:05:36 +0200

    add build

--- packages/generator-electron-app/lib/utils/rename-transform/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { ParsedPath } from 'gulp-rename';
-export declare type VariableTuple = [string, string] | undefined;
-export declare type PathTransform = (path: ParsedPath, variable?: VariableTuple, regExp?: RegExp) => ParsedPath;
-export declare type PathVariableTransform = [PathTransform, VariableTuple];
-//# sourceMappingURL=types.d.ts.map
