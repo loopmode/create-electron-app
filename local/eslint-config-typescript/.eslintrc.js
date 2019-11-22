66fe673530fcf816af24cab59b6fb70100a28d21
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-11-22 09:36:26 +0100

    add local eslint config packages

---
+++ local/eslint-config-typescript/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: ['plugin:@typescript-eslint/eslint-recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      1,
+      {
+        classes: false,
+        functions: false
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      1,
+      {
+        ignoreRestSiblings: true
+      }
+    ]
+  },
+  overrides: [
+    {
+      // https://github.com/typescript-eslint/typescript-eslint/issues/46#issuecomment-470486034
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
+      }
+    }
+  ]
+};
