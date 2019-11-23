b059f88aff0c44b8fe5f1a5b8b1f0ba1695a5d36
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-11-23 11:48:01 +0100

    formatting

--- packages/cea-gui/src/renderer/components/check-input/CheckInput.tsx
+++ packages/cea-gui/src/renderer/components/check-input/CheckInput.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Field } from 'formik';
 import cx from 'classnames';
-export const CheckInput: React.FC<
-  React.HTMLProps<HTMLInputElement> & {
-    type: 'radio' | 'checkbox';
-    label: string;
-    className?: string;
-  }
-> = props => {
+export const CheckInput: React.FC<React.HTMLProps<HTMLInputElement> & {
+  type: 'radio' | 'checkbox';
+  label: string;
+  className?: string;
+}> = props => {
   const { current: id } = React.useRef<string>(props.id || `${Math.random()}`);
   return (
     <>
--- packages/cea-gui/src/renderer/components/form-input/FormInput.tsx
+++ packages/cea-gui/src/renderer/components/form-input/FormInput.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Field } from 'formik';
 import cx from 'classnames';
-export const Input: React.FC<
-  React.HTMLProps<HTMLInputElement> & {
-    className?: string;
-  }
-> = props => <Field {...props} className={cx('input', props.className)} />;
+export const Input: React.FC<React.HTMLProps<HTMLInputElement> & {
+  className?: string;
+}> = props => <Field {...props} className={cx('input', props.className)} />;
