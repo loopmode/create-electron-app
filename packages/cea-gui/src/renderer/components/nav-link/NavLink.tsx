a7ddc113d7b64ce6c1d64fcb4c3f818ad26d75ad
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-11-26 07:38:14 +0100

    make navLink exact

--- packages/cea-gui/src/renderer/components/nav-link/NavLink.tsx
+++ packages/cea-gui/src/renderer/components/nav-link/NavLink.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { NavLink as RouterNavLink, NavLinkProps } from 'react-router-dom';
 import cx from 'classnames';
 
-const NavLinkButton: React.FC<NavLinkProps & { primary?: boolean }> = ({ primary, className, ...props }) => (
-  <NavLink className={cx('button', className, { 'is-primary': primary })} {...props} />
-);
+const NavLinkButton: React.FC<NavLinkProps & { primary?: boolean; success?: boolean }> = ({
+  primary,
+  success,
+  className,
+  ...props
+}) => <NavLink className={cx('button', className, { 'is-primary': primary, 'is-success': success })} {...props} />;
 
 export const NavLink: React.FC<NavLinkProps> & { Button: typeof NavLinkButton } = props => (
-  <RouterNavLink activeClassName="is-active" {...props} />
+  <RouterNavLink activeClassName="is-active" exact {...props} />
 );
 
 NavLink.Button = NavLinkButton;
